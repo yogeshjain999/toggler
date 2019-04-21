b97a626db5bc6aa151c55ac48245814f6efc70b5
Yogesh Khater <yogesh@joshsoftware.com>
2019-04-21 19:10:01 +0530

    Add TogglerCard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
 import Draggable from 'react-draggable';
 
@@ -39,7 +39,55 @@ const cards = [
     text: 'With supporting text below as a natural lead-in to additional content.',
     color: "",
   },
+  {
+    key: 7,
+    title: 'Special Title Treatment',
+    text: 'With supporting text below as a natural lead-in to additional content.',
+    color: "",
+  },
+  {
+    key: 8,
+    title: 'Special Title Treatment',
+    text: 'With supporting text below as a natural lead-in to additional content.',
+    color: "",
+  },
+  {
+    key: 9,
+    title: 'Special Title Treatment',
+    text: 'With supporting text below as a natural lead-in to additional content.',
+    color: "",
+  },
 ];
+
+const TogglerCard = ({ title, text }) => {
+  const [color, setColor] = useState('');
+
+  const onStop = (e, card) => {
+    if (card.lastX < 0) { // Left Toggle
+      setColor('danger');
+    } else if (card.lastX > 0) { // Right Toggle
+      setColor('success');
+    } else {
+      setColor('');
+    }
+  }
+
+  return (
+    <Draggable
+      axis="x"
+      scale={1}
+      defaultPosition={{ x: 0, y: 0 }}
+      grid={[50, 50]}
+      bounds={{ left: -50, right: 50 }}
+      onStop={onStop}
+    >
+      <Card body inverse color={color} style={{ backgroundColor: '#333', borderColor: '#333' }}>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{text}</CardText>
+      </Card>
+    </Draggable>
+  );
+}
 
 const App = () => (
   <div className="container-fluid">
@@ -47,16 +95,7 @@ const App = () => (
       cards.map(card => (
         <Row className="m-2" key={card.key}>
           <Col className="col-md-6 offset-md-3">
-            <Draggable
-              axis="x"
-              grid={[50, 50]}
-              bounds={{ left: -50, right: 50 }}
-            >
-              <Card body inverse color={card.color} style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                <CardTitle>{card.title}</CardTitle>
-                <CardText>{card.text}</CardText>
-              </Card>
-            </Draggable>
+            <TogglerCard title={card.title} text={card.text} />
           </Col>
         </Row>
       ))
